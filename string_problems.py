728b332910351fe96c2a48376a49ab5ae0303149
Tien Do <forever.love.wisdom@gmail.com>
2020-04-09 17:02:37 +0700

    Finish convert integers to strings

--- string_problems.py
+++ string_problems.py
@@ -18,15 +18,16 @@ def is_palindrome(str):
 def convert_strings_to_integers(str):
     """
     convert strings to integers without using int built-in function
+    strings -> integers
     "123" -> 123
     "000" -> 0
     """
     # build a hash table to convert string to integers
     # convert strings into array of chars
-    # loop through strings from the back
-    # use dispositional definitions of value
+    # reverse the array
+    # use dispositional definitions of decimal number
     # add values together
-    # return integer
+    # return the sum
 
     number_dict = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5,
                    '6': 6, '7': 7, '8': 8, '9': 9}
@@ -38,3 +39,59 @@ def convert_strings_to_integers(str):
         sum += num * (10**idx)
 
     return sum
+
+
+def convert_integers_to_strings(number):
+    """
+    Convert an integer to a string
+    int   -> string
+    123   -> "123"
+    0     -> "0"
+    99999 -> "9999"
+    """
+    # get each digit in the number by using a loop, modulus, and division
+    # operators.
+    # Then using a hash table for looking up.
+    # Then use a temporary array to store each char.
+    # Finally join them in a reverse way.
+    # Pay attention to special cases: 0, negative number,
+    # and really big number.
+
+    # if number == 0 or number == -0:
+    #     return '0'
+    # else:
+    #     negative = False
+    #     if number < 0:
+    #         number *= -1
+    #         negative = True
+    #     temp_dict = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6',
+    #                  7: '7', 8: '8', 9: '9'}
+    #     arr = []
+
+    #     while(number > 0):
+    #         temp_var = int(number % 10)
+    #         arr.append(temp_dict.get(temp_var))
+    #         number = int(number / 10)
+
+    # if negative:
+    #     return('-' + ''.join(reversed(arr)))
+    # else:
+    #     return ''.join(reversed(arr))
+    # Will return wrong result if the number is too big because of the int
+    # function
+
+    is_negative = False
+
+    if number < 0:
+        number, is_negative = -number, True
+
+    first_str = ord('0')
+    s = []
+
+    while True:
+        s.append(chr(first_str + number % 10))
+        number //= 10
+        if number == 0:
+            break
+
+    return ('-' if is_negative else '') + ''.join(reversed(s))
--- tests/test_practices.py
+++ tests/test_practices.py
@@ -13,4 +13,13 @@ def test_palindrome_false():
 def test_str_to_integer():
     assert string_problems.convert_strings_to_integers("123") == 123
     assert string_problems.convert_strings_to_integers("000") == 0
-    assert string_problems.convert_strings_to_integers("9999999111") == 9999999111
+    assert string_problems.convert_strings_to_integers("9999999111111111999999199999999119919191919191919191") == 9999999111111111999999199999999119919191919191919191
+
+
+def test_integer_to_str():
+    assert string_problems.convert_integers_to_strings(123) == "123"
+    assert string_problems.convert_integers_to_strings(0) == "0"
+    assert string_problems.convert_integers_to_strings(9999999111) == "9999999111"
+    assert string_problems.convert_integers_to_strings(-123) == "-123"
+    assert string_problems.convert_integers_to_strings(-1) == "-1"
+    assert string_problems.convert_integers_to_strings(-999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999) == "-999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999"
